39b7be94cfa242ff3bb7a7f81ed9a693fd6a1007
root <root@DESKTOP-7UUULE5.fritz.box>
2020-04-05 22:21:59 +0200

    Basic HTML Musik Player
    Musik Upload funktioniert. Wakka Wakka .mp3 zu testzwecken erstmal
    hardcoded.

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
-# SWE3
-## Simon und Dirks Projekt
+# SWE3 (JAMU - Java Music Player)
+## Funktionen:
+- Nur .mp3 Dateien sind erlaubt 
 
-## JAMU Java Music Player:
-- Spielt Musik
+## Installation:
+Um die hochgeladenen Musik Datein abspielen zu können muss die Wildfly Konfiguration angepasst werden.
+In der Standalone.xml jweils die zweite Zeile einfügen, welcome-content dient als Beispiel.
+
+    <location name="/" handler="welcome-content"/>
+    <location name="/musik" handler="musik"/>
+
+
+und unter handlers folgendes:
+
+    <file name="welcome-content" path="${jboss.home.dir}/welcome-content"/>
+    <file name="musik" path="${jboss.server.data.dir}/musikOrdner" directory-listing="true"/>
--- workspace/jamu/src/main/java/de/hsb/app/jamu/controller/MusikUploadHandler.java
+++ workspace/jamu/src/main/java/de/hsb/app/jamu/controller/MusikUploadHandler.java
@@ -3,31 +3,38 @@ package de.hsb.app.jamu.controller;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
+
+import org.apache.commons.io.FileUtils;
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.UploadedFile;
 
 import javax.faces.application.FacesMessage;
 
+import java.io.File;
 import java.io.Serializable;
 
 @ManagedBean
 @ViewScoped
 public class MusikUploadHandler implements Serializable {
 	private static final long serialVersionUID = 1L;
+	UploadedFile trackFile;
 
 	public void handleFileUpload(FileUploadEvent event) {
-		UploadedFile trackFile = event.getFile();
+		this.trackFile = event.getFile();
 		String trackName = event.getFile().getFileName();
-		String musikOrdnerPfad = FacesContext.getCurrentInstance().getExternalContext().getRequestContextPath();
 		
 		FacesMessage uploadErfolgMessage = new FacesMessage("Erfolg:", trackName + " wurde hochgeladen.");
 		FacesContext.getCurrentInstance().addMessage(null, uploadErfolgMessage);
 		
 		try {
-			trackFile.write(trackName);
-		} catch (Exception e1) {
+			String s = System.getProperty("jboss.server.data.dir"); //Kann ich als Final und Static declaren
+			File musikOrdner = new File(s + File.separator + "musikOrdner");
+	        musikOrdner.mkdirs();
+			File f = new File(musikOrdner, trackName);
+			FileUtils.writeByteArrayToFile(f, trackFile.getContents());
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
-			e1.printStackTrace();
+			e.printStackTrace();
 		}
 		
 	}
